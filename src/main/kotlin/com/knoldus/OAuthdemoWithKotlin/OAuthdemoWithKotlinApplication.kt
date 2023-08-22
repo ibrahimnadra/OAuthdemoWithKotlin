f6bf716a9badafd8b8d0d96a85834beb3d8f35a1
Ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-08-22 11:51:39 +0530

    Initial commit

---
+++ src/main/kotlin/com/knoldus/OAuthdemoWithKotlin/OAuthdemoWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.knoldus.OAuthdemoWithKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OAuthdemoWithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<OAuthdemoWithKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/knoldus/OAuthdemoWithKotlin/OAuthdemoWithKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knoldus.OAuthdemoWithKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OAuthdemoWithKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
